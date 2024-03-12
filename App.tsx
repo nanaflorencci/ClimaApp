d36ade323532b063b1ab98912025d9669da8f8ce
nanaflorencci <giovannaflorencci1814@gmail.com>
2024-03-12 15:43:45 -0300

    .

--- App.tsx
+++ App.tsx
@@ -1,23 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { Image, StyleSheet, Text, View } from "react-native";
 
 function App(): React.JSX.Element {
+
+  const [city, setCity] = useState <string>("Seul");
+  const [humidity, setHumidity] = useState <string>("15");
+  const [condition, setCondition] = useState <string>("Nublado");
+  const [rainProbabiliy, setRainProbabiliy] = useState <string>("7");
+  const [feelsLike, setFeelsLike] = useState <string>("16");
+  const [temperature, setTemperature] = useState <string>("19");
+  const [night, setNight] = useState <boolean>(false);
+
+  function isNight(){
+    const hour = new Date().getHours();
+    console.log(hour);
+    if(hour >= 18 || hour < 6){
+      setNight(true);
+    }else{
+      setNight(false);
+    }
+  }
+
+  useEffect (() => {
+    const intervalId = setInterval(()=>{
+      isNight();
+    },600);
+  })
+
   return(
-    <View style={styles.container}>
+    <View style={[styles.container, night ==false ? styles.containerBgDay : styles.containerBgNight]}>
       <View style={styles.header}>
-        <Text style={styles.city}>Presidente Epitácio</Text>
+        <Text style={styles.city}>{city}</Text>
       </View>
 
       <View style={styles.datailsContrainer}>
-        <Text style={styles.temperature}>13 °C</Text>
+        <Text style={styles.temperature}>{temperature} °C</Text>
         <Image source={require('./src/assets/images/icon3.png')} style={styles.weatherIcon}></Image>
       </View>
 
       <View>
-        <Text style={styles.weatherCondition}>Nublado</Text>
-        <Text style={styles.text}>Sensação térmica: 22 °C</Text>
-        <Text style={styles.text}>Probabilidade de chuva: 78%</Text>
-        <Text style={styles.text}>Umidade: 99%</Text>
+        <Text style={styles.weatherCondition}>{condition}</Text>
+        <Text style={styles.text}>Sensação térmica: {feelsLike} °C</Text>
+        <Text style={styles.text}>Probabilidade de chuva: {rainProbabiliy}%</Text>
+        <Text style={styles.text}>Umidade: {humidity}%</Text>
       </View>
         <Image source={require('./src/assets/images/cidade.png')} style={styles.bottomImage}/>
     </View>
@@ -30,7 +55,7 @@ const styles = StyleSheet.create({
     position: 'absolute',
     bottom: 0,
     resizeMode: 'cover',
-    height: '65%'
+    height: '63%'
   },
   header: {
     flexDirection: 'row',
@@ -40,7 +65,7 @@ const styles = StyleSheet.create({
   city: {
     fontWeight: 'bold',
     color: '#000000',
-    fontSize: 25
+    fontSize: 30
   },
   weatherIcon: {
     width: 100,
@@ -71,10 +96,16 @@ const styles = StyleSheet.create({
     backgroundColor: '#5c677d'
   },
   text: {
-    fontSize: 17,
+    fontSize: 20,
     fontWeight: 'bold',
     color: '#000000',
-    marginBottom: 10
+    marginBottom: 1
+  },
+  containerBgDay: {
+    backgroundColor: "#a2d2ff"
+  },
+  containerBgNight: {
+    backgroundColor: "#33415c"
   }
 });
 
